22ba67c06fad81415f693f7c0e2154aa2d5f8d43
ssyl96 <sidikisylla677@gmail.com>
2019-04-30 22:29:06 +0200

    update

--- source/Tempo.vhd
+++ source/Tempo.vhd
@@ -11,28 +11,32 @@ entity tempo is
 end tempo;
 
 architecture behac of tempo is
-    signal cpt : integer range 0 to 6000 := 0; -- 1MHz = 1us, 6ms / 1us = 6000
+    signal fin_s: std_logic;
+    signal cpt : integer range 0 to 5999 := 0; -- 1MHz = 1us, 6ms / 1us = 6000
 begin
     p0 : process( CLK_1MHz, reset )
     begin
         if reset = '1' then
+            fin_s <= '0';
             cpt <= 0;
         elsif rising_edge(CLK_1MHz) then
-            if cpt = 6000 and COM_TEMPO = '0' then
+            if cpt = 5999 and COM_TEMPO = '0' then
                 cpt <= 0;
-                fin <= '0';
+                fin_s <= '0';
             elsif COM_TEMPO = '1' then
-                if cpt = 6000 then 
-                    fin <= '1';
+                if cpt = 5999 then 
+                    fin_s <= '1';
                     cpt <= 0;
                 else 
                     cpt <= cpt + 1;
-                    fin <= '0';
+                    fin_s <= '0';
                 end if;
             else
-                fin <= '0';
+                fin_s <= '0';
             end if ;
         end if;
     end process ; -- p0
+    fin <= fin_s;
+
 
 end behac ; -- behac
