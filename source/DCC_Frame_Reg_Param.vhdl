099ac3bfeccef5bfc0d3dc66ddd77456c20cef4e
sylla <sylla>
2019-05-14 09:43:59 +0200

    update

--- source/DCC_Frame_Reg_Param.vhdl
+++ source/DCC_Frame_Reg_Param.vhdl
@@ -3,34 +3,36 @@ use ieee.std_logic_1164.all;
 
 ENTITY frame_reg IS
 	PORT(
-		din		: in std_logic_vector(32 downto 0);
+	    CLK_100MHz 	: in std_logic;
+        reset        : in std_logic;
+		din		: in std_logic_vector(31 downto 0);
 		din_addr: in std_logic(3 downto 0);
 		push	: in std_logic;
-
-		command_dcc : in std_logic_vector(31 downto 0);
-		command_dcc_v : in std_logic;
-
-		dout	: out std_logic_vector(32 downto 0)
+		dcc_control_out	: out std_logic_vector(31 downto 0);
+        dout	: out std_logic_vector(31 downto 0)
+		--command_dcc : in std_logic_vector(31 downto 0);
+		--command_dcc_v : in std_logic;		
 	);
 END frame_reg;
 
 architecture dataflow of frame_reg is
 
-signal r_param_0 : std_logic; -- addr train
-signal r_param_1 : std_logic; -- direction and  train speed
-signal r_param_2 : std_logic;
-signal r_param_3 : std_logic;
-signal r_param_4 : std_logic;
-signal r_param_5 : std_logic;
-signal r_param_6 : std_logic;
-signal r_control : std_logic;
+signal r_control : std_logic_vector (31 downto 0);
+signal r_param_1 : std_logic_vector (31 downto 0); 
+signal r_param_2 : std_logic_vector (31 downto 0);
+signal r_param_3 : std_logic_vector (31 downto 0);
+signal r_param_4 : std_logic_vector (31 downto 0);
+signal r_param_5 : std_logic_vector (31 downto 0);
+signal r_param_6 : std_logic_vector (31 downto 0);
 
 begin
 	process(din, push)
 		begin
-		if push = '1' then
-			fifo_d <= din;
-		end if;
+		case (din_addr) is
+		  when "0000" => dcc_control_out<= r_control;
+		  when  "0001" => dout<= r_param_1;
+		  when others      => dout<= r_param_2;
+		  end case;
 	end process;
-	dout <= fifo_d;
+	
 end dataflow;
