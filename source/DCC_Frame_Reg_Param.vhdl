e8f580d33acefabddc3bb1a2c695458c7a3b183a
ZHUANG Kailun <klp.zhuang@gmail.com>
2019-05-16 17:41:30 +0200

    Update Frame Reg Param

--- source/DCC_Frame_Reg_Param.vhdl
+++ source/DCC_Frame_Reg_Param.vhdl
@@ -2,17 +2,17 @@ LIBRARY ieee;
 use ieee.std_logic_1164.all;
 
 ENTITY frame_reg IS
-	PORT(
-	    CLK_100MHz 	: in std_logic;
-        reset        : in std_logic;
-		din		: in std_logic_vector(31 downto 0);
-		din_addr: in std_logic(3 downto 0);
-		push	: in std_logic;
-		dcc_control_out	: out std_logic_vector(31 downto 0);
-        dout	: out std_logic_vector(31 downto 0)
-		--command_dcc : in std_logic_vector(31 downto 0);
-		--command_dcc_v : in std_logic;		
-	);
+PORT(
+	CLK_100MHz 		: in std_logic;
+	reset       	: in std_logic;
+	wdata			: in std_logic_vector(31 downto 0);
+	wdata_addr		: in std_logic_vector(3 downto 0);
+	wdata_v			: in std_logic;
+	rdata_addr		: in std_logic_vector(3 downto 0);
+	rdata_v			: in std_logic;
+	dcc_control_out	: out std_logic_vector(31 downto 0);
+	dout		: out std_logic_vector(31 downto 0)
+);
 END frame_reg;
 
 architecture dataflow of frame_reg is
@@ -25,14 +25,25 @@ signal r_param_4 : std_logic_vector (31 downto 0);
 signal r_param_5 : std_logic_vector (31 downto 0);
 signal r_param_6 : std_logic_vector (31 downto 0);
 
+-- signal r_control_v : std_logic; -- for v2
+
 begin
-	process(din, push)
-		begin
-		case (din_addr) is
-		  when "0000" => dcc_control_out<= r_control;
-		  when  "0001" => dout<= r_param_1;
-		  when others      => dout<= r_param_2;
-		  end case;
+	process(CLK_100MHz, reset)
+	begin
+	if reset = '1' then
+		r_control <= X"00000000";
+		r_param_1 <= X"00000000";
+	elsif rising_edge(CLK_100MHz) then
+		if wdata_v = '1' then
+		case wdata_addr is
+			when "0000" => r_control <= wdata;
+			when "0001" => r_param_1 <= wdata;
+			when others => NULL;
+		end case;
+		end if;
+	end if;
+
 	end process;
-	
+dcc_control_out <= r_control;
+dout <= r_param_1;
 end dataflow;
